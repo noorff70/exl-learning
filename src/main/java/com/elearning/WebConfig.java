cb60b35c6f765f63eacdc0b896ca8595d873bffc
NoorF <NoorF@IS7215LT.tmmc.na.corp.toyota.com>
2020-04-10 19:04:35 -0400

    DB configuration.

--- src/main/java/com/elearning/WebConfig.java
+++ src/main/java/com/elearning/WebConfig.java
@@ -10,7 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+/*
+ * db configuration
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
